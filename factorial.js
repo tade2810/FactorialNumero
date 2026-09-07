f2b5798566d068edfd0ee5085ff0877e20cb7ac5
tade <vaxel7045@gmail.com>
2026-09-07 10:43:54 -0600

    Añadiendo codigo

---
+++ factorial.js
@@ -0,0 +1,15 @@
+function factorial(n) {
+    if (n < 0) {
+        return "El factorial no está definido para números negativos";
+    }
+
+    let resultado = 1;
+
+    for (let i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+
+    return resultado;
+}
+
+console.log(factorial(5)); // 120
