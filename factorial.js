b3c3241c328ab6c820e6ff6ec994d310be975794
Tade Lop <vaxel7045@gmail.com>
2026-09-08 12:38:30 -0600

    Update factorial

--- factorial.js
+++ factorial.js
@@ -15,3 +15,21 @@ function factorial(n) {
 console.log(factorial(5)); // 120
 console.log(factorial(10)); // 120
 console.log(factorial(50)); // 120
+
+function factorial2(n) {
+    if (n < 0) {
+        return "El factorial no está definido para números negativos";
+    }
+
+    let resultado = 1;
+
+    for (let i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+
+    return resultado;
+}
+
+console.log(factorial2(5)); // 120
+console.log(factorial2(10)); // 120
+console.log(factorial2(50)); // 120
